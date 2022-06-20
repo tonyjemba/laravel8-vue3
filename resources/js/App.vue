97f1971bdacd1a6fc685fed36d8ce11bbe64ab9e
Tony Jemba <jjembatony@gmail.com>
2022-06-20 23:58:48 +0300

    Update App.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,13 @@
+<script setup>
+const heloo = "Hello vue";
+<script>
 <template>
     <div class="p-6 max-w-sm mx-auto bg-indigo-50 rounded-xl shadow-md flex items-center space-x-4">
   <div class="flex-shrink-0">
     <img class="h-12 w-12" src="/chrome.svg" alt="ChitChat Logo">
   </div>
   <div>
-    <div class="text-xl font-medium text-black">Chrome</div>
+    <div class="text-xl font-medium text-black">{{heloo}}</div>
     <p class="text-gray-500">Include views, databases, relationships</p>
   </div>
 </div>
